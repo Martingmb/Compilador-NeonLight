0f464bf0f5a111c3b2eddf5fd1e04376000f3cbb
Daniel Saldaña <danielsaldana@Daniels-MacBook-Pro.local>
2019-10-21 21:22:27 -0500

    Proyecto base IDE

---
+++ Neonlight/Neonlight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Neonlight
+//
+//  Created by Daniel Saldaña on 10/21/19.
+//  Copyright © 2019 Daniel Saldaña. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
